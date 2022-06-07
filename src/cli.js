239e10f41a6c2491c5a397021b571e35f84f4cc4
nikita.b <bulatov.nre@gmail.com>
2022-06-07 20:35:21 +0300

    update cli.js

--- src/cli.js
+++ src/cli.js
@@ -1,9 +1,9 @@
 import readlineSync from 'readline-sync';
 
 const askName = () => {
-    console.log('What is your name? ');
-    const name = readlineSync.question('May I have your name?: ');
-    console.log(`Hello, ${name}!`);
-    return name;
-}
+  console.log('What is your name? ');
+  const name = readlineSync.question('May I have your name?: ');
+  console.log(`Hello, ${name}!`);
+  return name;
+};
 export default askName;
