b8513ef2f459537d14e8613506757ead43248ed0
nikita.b <bulatov.nre@gmail.com>
2022-06-13 17:50:36 +0300

    add brain-calc game

---
+++ src/games/brain-calc.js
@@ -0,0 +1,35 @@
+import getRandomNumber from '../getRandomNumber.js';
+import gameInit from '../index.js';
+
+const expressions = ['-', '+', '*'];
+
+const calculateExpression = (firstNumber, secondNumber, expression) => {
+  switch (expression) {
+    case '-':
+      return firstNumber - secondNumber;
+    case '+':
+      return firstNumber + secondNumber;
+    case '*':
+      return firstNumber * secondNumber;
+    default:
+      console.log('error: unknown expression');
+      return 0;
+  }
+};
+
+const createRound = () => {
+  const firstNumber = getRandomNumber(0, 101);
+  const secondNumber = getRandomNumber(0, 101);
+  const expression = expressions[getRandomNumber(0, expressions.length)];
+
+  const correctAnswer = calculateExpression(firstNumber, secondNumber, expression);
+  const questionNumber = `${String(firstNumber)} ${expression} ${String(secondNumber)}`;
+
+  return [questionNumber, String(correctAnswer)];
+};
+
+const gameStart = () => {
+  const gameDesc = 'What is the result of the expression?';
+  gameInit(gameDesc, createRound);
+};
+export default gameStart;
