95791f81dcc00bd403ea6994ea2b91564b9d40a7
nikita.b <bulatov.nre@gmail.com>
2022-06-24 18:53:51 +0300

    refactoring

--- src/cli.js
+++ src/cli.js
@@ -1,6 +1,7 @@
 import readlineSync from 'readline-sync';
 
 const askName = () => {
+  console.log('Welcome to the Brain Games!');
   console.log('What is your name? ');
   const name = readlineSync.question('May I have your name?: ');
   console.log(`Hello, ${name}!`);
